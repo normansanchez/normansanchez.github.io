85e4e17ae1054eed4aa71fbae4a6934fd8f50dd9
normansanchez <normansanchez@hotmail.com>
2022-10-30 20:44:17 -0300

    Add files via upload

---
+++ codigoJavaScript/miCodigo.js
@@ -0,0 +1,19 @@
+function aRojo() {
+  document.getElementById("titulo").style.color = "#FF0000";
+  alert("Cambiaste el color a Rojo")
+}
+
+function aAzul() {
+  document.getElementById("titulo").style.color = "#1017D1";
+  alert("Cambiaste el color a Azul")
+}
+
+function aAmarillo() {
+  document.getElementById("titulo").style.color = "#FCE94F";
+  alert("Cambiaste el color a Amarillo")
+}
+
+function aVerde() {
+  document.getElementById("titulo").style.color = "#4B8315";
+  alert("Cambiaste el color a Verde")
+}
